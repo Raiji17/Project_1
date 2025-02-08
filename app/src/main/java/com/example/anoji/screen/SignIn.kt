1acb99e2e0015cfc920460a5361f63ffbe71313f
Raiji17 <Raihananoji8@gmail.com>
2025-02-08 20:45:20 +0700

    tambahan sign in, sign up

--- app/src/main/java/com/example/anoji/screen/SignIn.kt
+++ app/src/main/java/com/example/anoji/screen/SignIn.kt
@@ -1,6 +1,5 @@
 package com.example.anoji.screen
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
